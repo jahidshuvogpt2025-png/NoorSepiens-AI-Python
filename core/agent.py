ac9f61c990bbcbb40b42889756158fda57829f03
jahidshuvogpt2025-png <jahidshuvogpt2025@gmail.com>
2026-07-23 18:56:28 +0600

    Update agent.py

--- core/agent.py
+++ core/agent.py
@@ -1,4 +1,5 @@
 from ai.router import AIRouter
+from core.memory import Memory
 
 
 
@@ -11,27 +12,61 @@ class NoorAgent:
 
         self.ai = AIRouter()
 
+        self.memory = Memory()
+
 
 
     def response(self, text):
 
 
+        # Save user message
+
+        self.memory.save(text)
+
+
+
+        history = self.memory.get()
+
+
+
+        context = "\n".join(history)
+
+
+
         prompt = f"""
+
 You are {self.name}.
 
 Reply in Bangla.
 
-User message:
+Previous conversation:
+
+{context}
+
+
+Current user message:
 
 {text}
 
+
 Be helpful, friendly and intelligent.
+
 """
+
 
 
         reply = self.ai.ask(
             prompt
         )
+
+
+
+        # Save AI reply
+
+        self.memory.save(
+            "Assistant: " + reply
+        )
+
 
 
         return reply

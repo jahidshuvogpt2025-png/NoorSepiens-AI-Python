60629093e8ee94378a30d70e29163bcc67815179
jahidshuvogpt2025-png <jahidshuvogpt2025@gmail.com>
2026-07-23 18:54:37 +0600

    Update router.py

--- ai/router.py
+++ ai/router.py
@@ -1,45 +1,78 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 
 class AIRouter:
 
+
     def __init__(self):
-        self.api_key = os.getenv("OPENROUTER_API_KEY")
+
+        self.api_key = os.getenv(
+            "OPENROUTER_API_KEY"
+        )
+
 
 
     def ask(self, message):
 
+
         url = "https://openrouter.ai/api/v1/chat/completions"
 
+
+
         headers = {
-            "Authorization": f"Bearer {self.api_key}",
-            "Content-Type": "application/json"
+
+            "Authorization":
+            f"Bearer {self.api_key}",
+
+            "Content-Type":
+            "application/json"
+
         }
+
 
 
         data = {
 
-            "model": "openai/gpt-4o-mini",
+            "model":
+            "openai/gpt-4o-mini",
 
-            "messages": [
+
+            "messages":[
+
                 {
-                    "role": "user",
-                    "content": message
+
+                    "role":"user",
+
+                    "content":message
+
                 }
+
             ]
 
         }
 
 
+
         response = requests.post(
+
             url,
+
             headers=headers,
+
             json=data
+
         )
 
 
+
         result = response.json()
+
 
 
         return result["choices"][0]["message"]["content"]

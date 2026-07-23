e015e2aa2544d934aa58c1b039a49c7ad3710416
jahidshuvogpt2025-png <jahidshuvogpt2025@gmail.com>
2026-07-23 18:45:49 +0600

    Create router.py

---
+++ ai/router.py
@@ -0,0 +1,45 @@
+import os
+import requests
+
+
+class AIRouter:
+
+    def __init__(self):
+        self.api_key = os.getenv("OPENROUTER_API_KEY")
+
+
+    def ask(self, message):
+
+        url = "https://openrouter.ai/api/v1/chat/completions"
+
+        headers = {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json"
+        }
+
+
+        data = {
+
+            "model": "openai/gpt-4o-mini",
+
+            "messages": [
+                {
+                    "role": "user",
+                    "content": message
+                }
+            ]
+
+        }
+
+
+        response = requests.post(
+            url,
+            headers=headers,
+            json=data
+        )
+
+
+        result = response.json()
+
+
+        return result["choices"][0]["message"]["content"]

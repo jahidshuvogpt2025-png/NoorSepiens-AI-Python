5cae2fadb370fbbb1f63325b9c2b4b9c1e5960db
jahidshuvogpt2025-png <jahidshuvogpt2025@gmail.com>
2026-07-23 19:30:30 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
+import sys
+import os
+
+# project root path add
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+
 from telegram.bot import run_bot
+
 
 
 def main():
 
-    print(
-        "Starting NoorSepiens AI Python 🚀"
-    )
-
+    print("Starting NoorSepiens AI Python 🚀")
 
     run_bot()
 

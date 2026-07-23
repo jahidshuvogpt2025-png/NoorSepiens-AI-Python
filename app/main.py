380ce7243a59f855dcef47999817255001ab73b7
jahidshuvogpt2025-png <jahidshuvogpt2025@gmail.com>
2026-07-23 18:51:26 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-print("NoorSepiens AI Python Started 🚀")
+from telegram.bot import run_bot
+
+
+def main():
+
+    print(
+        "Starting NoorSepiens AI Python 🚀"
+    )
+
+
+    run_bot()
+
+
+
+if __name__ == "__main__":
+
+    main()
